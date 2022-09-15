2164e4fac09352b7363870f20c7957f6d57e1716
Yangsong <861868991@qq.com>
2022-09-15 17:46:19 +0800

    B包大于第三关关闭里程碑

--- Client/AnswerType/Assets/Scripts/SUIFW/Diplomats/Main/UI_IF_MainPosition.cs
+++ Client/AnswerType/Assets/Scripts/SUIFW/Diplomats/Main/UI_IF_MainPosition.cs
@@ -60,7 +60,7 @@ public partial class UI_IF_Main
     protected void RefreshPosition(EventParam param)
     {
         //b包关卡大于5关闭里程碑
-        if (!GL_CoreData._instance.AbTest && GL_PlayerData._instance.CurLevel > 5)
+        if (!GL_CoreData._instance.AbTest && GL_PlayerData._instance.CurLevel > 3)
         {
             _tfLevelSlider.SetActive(false);
             return;
