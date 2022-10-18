aa4632a26c4713f774971dc9659b171479967640
shyf2583536 <shyf2583536@163.com>
2022-10-18 14:29:50 +0800

    01.18 更新显示

--- Client/AnswerType/Assets/Scripts/SUIFW/Diplomats/Withdraw/UI_IF_WithdrawSuccess.cs
+++ Client/AnswerType/Assets/Scripts/SUIFW/Diplomats/Withdraw/UI_IF_WithdrawSuccess.cs
@@ -75,16 +75,15 @@ public class UI_IF_WithdrawSuccess : BaseUIForm
             if (GL_CoreData._instance.AbTest)
             {
                 _tipsText.SetActive(true);
-            
+                _tipsText.text = string.Format(_list[1], _money, (_withDrawResult - _money).ToString("0.00"));
                 if (_withDrawResult <=_money )
                 {
-                    _tipsText.text = string.Format(_list[1], _money, (_withDrawResult - _money).ToString("0.00"));
+                    _tipsText.SetActive(false);
                     int hour = (GL_PlayerData._instance._WithDrawGrowConfig.countDown / 3600);
                     int min = (GL_PlayerData._instance._WithDrawGrowConfig.countDown -
                                (hour*60)) / 60;
                     int second = GL_PlayerData._instance._WithDrawGrowConfig.countDown - (hour * 60) - (min * 60);
-                    UI_HintMessage._.ShowMessage($"当前福利放完毕\n{hour.ToString("00")}" + $":{min.ToString("00")}" + $"{second.ToString("00")}" + $"小时后继续发放");
-                    _tipsText.SetActive(false);
+                    UI_HintMessage._.ShowMessage($"当前福利放完毕\n{hour.ToString("00")}" + $":{min.ToString("00")}" + $"{second.ToString("00")}" + $"后继续发放");
                 }
             }
             else
